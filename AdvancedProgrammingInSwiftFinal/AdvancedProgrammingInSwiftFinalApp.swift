1e9a39db32219ed34bae4ede2bba96ba47134c64
Glendon Philipp Baculio <glendonphilippbaculio@gmail.com>
2023-04-26 01:58:01 +0800

    Initial Commit

---
+++ AdvancedProgrammingInSwiftFinal/AdvancedProgrammingInSwiftFinalApp.swift
@@ -0,0 +1,17 @@
+//
+//  AdvancedProgrammingInSwiftFinalApp.swift
+//  AdvancedProgrammingInSwiftFinal
+//
+//  Created by Glendon Philipp Baculio on 4/26/23.
+//
+
+import SwiftUI
+
+@main
+struct AdvancedProgrammingInSwiftFinalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
